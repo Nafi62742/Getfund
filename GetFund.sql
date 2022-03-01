4aa0fe133c7be9e0d7ecb0b83f2436d6144f82ea
Nafi62742 <61117249+Nafi62742@users.noreply.github.com>
2022-03-01 17:27:23 +0600

    Fix

---
+++ GetFund.sql
@@ -0,0 +1,60 @@
+create database GetFund
+Drop database GetFund
+
+create table GUser
+(
+ID int IDENTITY(1,1) PRIMARY KEY,
+Email varchar(50) NULL,
+Password varchar(50) NOT NULL CHECK (LEN(Password) >= 6),
+IsValid Bit default 'FALSE',
+);
+
+
+INSERT INTO GUser(Email,Password,IsValid)
+VALUES ('B@gmail.com','123456','TRUE');
+
+Select * from GUser
+
+create table Profile
+(
+ID int foreign key REFERENCES GUser (ID),
+Name varchar(50) NOT NUll,
+Address varchar(50) default 'Dhaka',
+NID int  NULL
+);
+
+create table Project
+(
+PId int IDENTITY(1,1) PRIMARY KEY,
+ID int foreign key REFERENCES GUser (ID),
+Title varchar(50) NOT NUll,
+Info varchar(50) NOT NULL,
+VideoLink varchar(250) NOT NULL,
+Type varchar(50) NOT NULL,
+Target varchar(50),
+MoneyRaised float default 0.00
+);
+Select * From Project
+
+create table Comments
+(
+ID int foreign key REFERENCES GUser (ID),
+PId int foreign key REFERENCES Project (PId),
+Comment varchar(50) NOT NULL,
+Moment DATETIME  NOT NUll,
+Amount float Not Null
+);
+
+create table Donation
+(
+DonationId int IDENTITY(1,1) PRIMARY KEY,
+ID int foreign key REFERENCES GUser (ID),
+PId int foreign key REFERENCES Project (PId),
+DonateDes varchar(50) NOT NULL,
+DonateTime DATETIME NOT NUll,
+transaction_id int NOT NUll,
+Amount float Not Null
+);
+
+
+DELETE FROM GUser WHERE ID=4;
---
+++ Getfund/Controllers/HomeController.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using Getfund.Models;
+
+
+namespace Getfund.Controllers
+{
+    public class HomeController : Controller
+    {
+        GetFundEntities db = new GetFundEntities();
+        public static int idUser;
+        public static String userEmail;
+        public static String userName;
+        public static bool emailexists=false;
+        public ActionResult Index()
+        {
+            List<Project> projects = db.Projects.ToList();
+            return View(projects);
+              
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+        
+        public ActionResult Search(string email, string LoginPass)
+        {
+
+            if (Session["IdUsSS"] != null)
+            {
+                var pro = (from g in db.GUsers
+                           join p in db.Profiles on g.ID equals p.ID
+                           where g.Email == userEmail
+                           select new ProfileShow
+                           {
+                               ID = g.ID,
+                               Name = p.Name,
+                               Address = p.Address,
+                               Email = g.Email,
+                               NID = p.NID,
+                           }).SingleOrDefault();
+                ViewBag.Message = pro.Name;
+                List<Project> projects = db.Projects.ToList();
+                return View(projects);
+
+            }
+            else
+            {List<GUser> Users = db.GUsers.Where(temp => temp.Email.Equals(email) && temp.Password.Equals(LoginPass)).ToList();
+
+                if (Users.Count > 0)
+                {
+
+                    var pro = (from g in db.GUsers
+                               join p in db.Profiles on g.ID equals p.ID
+                               where g.Email == email
+                               select new ProfileShow
+                               {
+                                   ID = g.ID,
+                                   Name = p.Name,
+                                   Address = p.Address,
+                                   Email = g.Email,
+                                   NID = p.NID,
+                               }).SingleOrDefault();
+                    idUser = pro.ID;
+                    userEmail = pro.Email;
+                    ViewBag.Message = pro.Name;
+                    Session["IdUsSS"] = pro.ID.ToString();
+                    Session["UsernameSS"] = pro.Name.ToString();
+                    Session["UserEmail"] = pro.Email.ToString();
+
+                    List<Project> projects = db.Projects.ToList();
+                    return View(projects);
+                }
+                else
+                {
+                    TempData["LoginError"] = "Check password or username!";
+                    return RedirectToAction("LoginPage");
+
+                }
+            }
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(GUser user)
+        {
+
+            List<GUser> Users = db.GUsers.Where(temp => temp.Email.Equals(user.Email)).ToList();
+
+            if (Users.Count < 1)
+            {
+                db.GUsers.Add(user);
+                db.SaveChanges();
+                BuildEmailTemplate(user.ID);
+                return RedirectToAction("LoginPage");
+            }
+            else
+            {
+                TempData["LoginError"] = "Check password or username!";
+                return RedirectToAction("Register");
+
+            }
+
+        }
+        public ActionResult Logout()
+        {
+            Session["IdUsSS"] = null;
+            Session["UsernameSS"] = null;
+            Session["UserEmail"] = null;
+            return RedirectToAction("Index");
+        
+        }
+        [HttpGet]
+        public ActionResult LoginPage()
+        {
+            if (Session["IdUsSS"]!=null)
+            {
+                return RedirectToAction("Search");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        public ActionResult LoginPage(GUser user)
+        {
+            if(user == null)
+            {
+                return View();
+            }
+            else
+            {
+            db.GUsers.Add(user);
+            db.SaveChanges();
+            BuildEmailTemplate(user.ID);
+            return RedirectToAction("LoginPage");
+            }
+            
+           
+        }
+      
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            var pro = (from g in db.GUsers
+                       join p in db.Profiles on g.ID equals p.ID
+                       where g.ID == idUser
+                       select new ProfileShow
+                       {
+                           ID=g.ID,
+                           Name = p.Name,
+                           Address = p.Address,
+                           Email = g.Email,
+                           NID = p.NID,
+                       }).SingleOrDefault();
+            if(pro == null)
+            {
+                return RedirectToAction("LoginPage");
+            }
+            else
+            {
+                 return View(pro);
+            }
+            
+        }
+        public ActionResult Project()
+        {
+            List<Project> projects = db.Projects.ToList();
+            return View(projects);
+        }
+
+        public ActionResult Details(int BoxID)
+        {
+            var Detail = (from pj in db.Projects
+                       join p in db.Profiles on pj.ID equals p.ID
+                       where pj.PId == BoxID
+                       select new ShowDetails
+                       {
+                           ID = p.ID,
+                           Name = p.Name,
+                           Address = p.Address,
+                           NID = p.NID,
+                           Title = pj.Title,
+                          Info=pj.Info,
+                          Type=pj.Type,
+                          Target=pj.Target,
+                          MoneyRaised=pj.MoneyRaised,
+                       }).SingleOrDefault();
+            return View(Detail);
+        }
+        
+        public ActionResult Confirm(int regId)
+        {
+            ViewBag.regID = regId;
+            return View();
+        }
+        public JsonResult RegistrationConfirm(int regId)
+        {
+            GUser Data = db.GUsers.FirstOrDefault(x => x.ID == regId);
+            Data.IsValid = true;
+            db.SaveChanges();
+            var msg = "Your Email is varified.";
+            return Json(msg,JsonRequestBehavior.AllowGet);
+            
+        }
+        
+
+        private void BuildEmailTemplate(int RegiD)
+        {
+            string body = System.IO.File.ReadAllText(HostingEnvironment.MapPath("~/EmailTemplate/")+"GetFundMail"+".cshtml");
+            //throw new NotImplementedException();
+            var regInfo =db.GUsers.Where(temp =>temp.ID==RegiD).FirstOrDefault();
+            var url = "https://localhost:44336/" + "Home/Confirm?regId=" + RegiD;
+            body =body.Replace("@ViewBag.ConfirmationLink",url);
+            body =  body.ToString();
+
+            BuildEmailTemplate("Your account is verified",body,regInfo.Email);
+        }
+
+        private void BuildEmailTemplate(string SubjectText, string TextBody, string EmailTo)
+        {
+            string from,to,cc,bcc, subject,body;
+            from = "botracer007@gmail.com";
+            to = EmailTo.Trim();
+            bcc = "";
+            cc = "";
+            subject = SubjectText;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(TextBody);
+            body= sb.ToString();
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(from);
+            mail.To.Add(new MailAddress(to));
+            if (!String.IsNullOrEmpty(bcc))
+            {
+                mail.Bcc.Add(new MailAddress(bcc));
+            }
+            if (!String.IsNullOrEmpty(cc))
+            {
+                mail.CC.Add(new MailAddress(cc));
+            }
+            mail.Subject = SubjectText;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+            SendMail(mail);
+        }
+
+        private void SendMail(MailMessage mail)
+        {
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.UseDefaultCredentials = false;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Credentials = new System.Net.NetworkCredential("racerkenway318@gmail.com", "Ghum1234");
+            client.EnableSsl = true;
+            try
+            {
+                client.Send(mail);
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
